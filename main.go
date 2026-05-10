57e12a2a4c0b5c36e77909b5a715c6db6db97084
CyberGrim <cybergrim99@gmail.com>
2026-05-10 13:25:33 -0400

    Adding a simple HTML page to be served by Chirpy

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	srv.ListenAndServe()
+	serveErr := srv.ListenAndServe()
+	if serveErr != nil {
+		log.Fatal(serveErr)
+	}
 }

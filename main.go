79eacee5b43b7d49d38e0acccabefa2a89c80228
CyberGrim <cybergrim99@gmail.com>
2026-05-18 13:08:48 -0400

    Added POST and GET restrictions based on Boot.dev lesson assignment

--- main.go
+++ main.go
@@ -38,9 +38,9 @@ func main() {
 	api := &apiConfig{}
 
 	mux.Handle("/app/", http.StripPrefix("/app", api.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/metrics", api.handlerMetrics)
-	mux.HandleFunc("/reset", api.handlerReset)
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /metrics", api.handlerMetrics)
+	mux.HandleFunc("POST /reset", api.handlerReset)
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))

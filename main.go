81f512ee1a5a07e05bebc9b87b2b1edf0baad55d
CyberGrim <cybergrim99@gmail.com>
2026-05-20 20:25:20 -0400

    Added /api/ prefeix to api endpoints

--- main.go
+++ main.go
@@ -38,9 +38,9 @@ func main() {
 	api := &apiConfig{}
 
 	mux.Handle("/app/", http.StripPrefix("/app", api.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("GET /metrics", api.handlerMetrics)
-	mux.HandleFunc("POST /reset", api.handlerReset)
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/metrics", api.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", api.handlerReset)
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
